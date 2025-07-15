02642ed5e24309d38ffb3bdd7ca1d8bd67f99c29
bokykisac <dervy97@gmail.com>
2025-07-15 15:53:29 +0200

    login

--- test/pageobjects/login.page.ts
+++ test/pageobjects/login.page.ts
@@ -1,41 +1,29 @@
-import { $ } from '@wdio/globals'
-import Page from './page';
+import { $ } from "@wdio/globals";
+import Page from "./page";
 
-/**
- * sub page containing specific selectors and methods for a specific page
- */
 class LoginPage extends Page {
-    /**
-     * define selectors using getter methods
-     */
-    public get inputUsername () {
-        return $('#username');
-    }
+  get emailInput() {
+    return $('[name="username"]');
+  }
+  get continueBtn() {
+    return $("#login-submit");
+  }
+  get passwordInput() {
+    return $("#password");
+  }
 
-    public get inputPassword () {
-        return $('#password');
-    }
+  public async login(email: string, password: string) {
+    await this.emailInput.setValue(email);
+    await this.continueBtn.click();
 
-    public get btnSubmit () {
-        return $('button[type="submit"]');
-    }
+    await this.passwordInput.waitForDisplayed();
+    await this.passwordInput.setValue(password);
+    await this.continueBtn.click();
+  }
 
-    /**
-     * a method to encapsule automation code to interact with the page
-     * e.g. to login using username and password
-     */
-    public async login (username: string, password: string) {
-        await this.inputUsername.setValue(username);
-        await this.inputPassword.setValue(password);
-        await this.btnSubmit.click();
-    }
-
-    /**
-     * overwrite specific options to adapt it to page object
-     */
-    public open () {
-        return super.open('login');
-    }
+  public open() {
+    return super.open("https://id.atlassian.com/login");
+  }
 }
 
 export default new LoginPage();
